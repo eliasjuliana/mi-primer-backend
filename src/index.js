c2910e7d8309831030534df06ddd44b9a4f29972
Juliana Elias <eliasjuliana@gmail.com>
2023-10-24 15:50:55 -0300

    configuraciones iniciales

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('hola mundo desde el backend!!!')
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+//1. Iniciar la aplicacion
+const app = express();
+
+//2. Configuraciones generales
+const PORT = process.env.PORT || 5000;
+
+console.log(process.env);
+
+//3. Middlewares
+app.use(morgan("dev"));
+app.use(cors()); //CROSS-ORIGIN-RESOURCE-SHARING
+app.use(express.json());
+
+//4.Rutas
+
+//5. Iniciar el loop del servidor
+app.listen(()=>{
+    console.log(`Servidor ejecutandose en el puerto ${PORT}`)
+})
