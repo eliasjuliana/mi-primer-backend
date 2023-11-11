f0422055a7e284ddc914d45bb44b6e07f273a6f7
Juliana Elias <eliasjuliana@gmail.com>
2023-11-11 11:50:55 -0300

    corrijo payload del token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
 import { postLogin } from '../controllers/authController.js';
+import { validateBody } from '../middlewares/validateBody.js';
 
 const router = express.Router();
 
-router.post('/login', postLogin);
+router.post('/login', (request, response, next) => validateBody(request, response, next, ), postLogin);
 
 export default router;
